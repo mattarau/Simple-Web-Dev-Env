d70c93818b22e8af32ecca8c7d4dfe9938b8aeae
Matheus Araujo <matheus.araujo@e-strident.com>
2015-01-25 22:45:12 -0300

    Update Node.Js installation

--- provision.sh
+++ provision.sh
@@ -12,14 +12,9 @@ apt-get install -y git git-flow
 git config --global url."https://".insteadOf git://
 
 echo "Installing Node.js and npm"
-#Instructions as per https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager (Vagrant runs in root/sudo mode already)
-apt-get install -y python-software-properties python g++ make
-# Default ubuntu version seems to be 0.6, latest as of July 2013 is 0.10.13
-# chris-lea is tracking latest node version, so we use that
-add-apt-repository ppa:chris-lea/node.js
-apt-get update
-apt-get install -y nodejs
-# nodejs above includes npm --- apt-get install -y npm
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+
 echo "installing Ruby and Compass"
 # Ruby
 curl -L https://get.rvm.io | bash -s stable
